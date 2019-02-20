c668a3a696c08d1fc678515d0b6f1dd2d3fe99f1
Vyacheslav_K <vkraskovsky@gmail.com>
2019-02-20 16:06:59 +0300

    Lesson 03 Modified

--- Lesson 03/Task02.py
+++ Lesson 03/Task02.py
@@ -1,20 +1,21 @@
-itr=int(input('Number of inerations:'))
-list=[]
-list_new=[]
-#Getting the list and printing the item to exclude
+# Задача 1
+# Исключить из списка А1..AN максимальный элемент.
+
+itr = int(input('Number of iterations:'))
+my_list = []
+list_new = []
+# Getting the list and printing the item to exclude
 for i in range(itr):
-    list.append(input('Type item'+ str(i+1) + ' :'))
-print('Max item to exclude:',max(list))
-#Variant1 using append method
-for x in range(len(list)):
-    if list[x]!=max(list):
-        list_new.append(list[x])
+    my_list.append(input('Type item' + str(i + 1) + ' :'))
+print('Max item to exclude:', max(my_list))
+# Variant1 using append method
+for x in range(len(my_list)):
+    if my_list[x] != max(my_list):
+        list_new.append(my_list[x])
 print(list_new)
-#Variant2 using slices
-y=0
-while (y<=len(list)) and (list[y]!=max(list)):
-    y=y+1
-list_final=list[:y] + list[y+1:]
+# Variant2 using slices
+y = 0
+while (y <= len(my_list)) and (my_list[y] != max(my_list)):
+    y = y + 1
+list_final = my_list[:y] + my_list[y + 1:]
 print(list_final)
-
-
--- Lesson 03/Task06.py
+++ Lesson 03/Task06.py
@@ -1,6 +1,9 @@
-str1=input('Print the first string')
-str2=input('Print the second string')
-if str1.find(str2)!=-1:
+# Задача 5
+# На вход поступают две строки. Необходимо выяснить, является ли вторая строка подстрокой первой.
+
+str1 = input('Print the first string')
+str2 = input('Print the second string')
+if str1.find(str2) != -1:
     print('The second string is a part of the first one!')
 else:
     print('No matches found!')
