9aa071f6c13f3a92a85db7e79d5a93901a9cd58c
vkraskovsky <vkraskovsky@gmail.com>
2019-03-16 00:07:40 +0300

    added match and player

--- matches_site/search_matches/admin.py
+++ matches_site/search_matches/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Team
+from .models import Team, Player, Match
 
 # Register your models here.
 
 admin.site.register(Team)
+admin.site.register(Player)
+admin.site.register(Match)
--- matches_site/search_matches/models.py
+++ matches_site/search_matches/models.py
@@ -9,3 +9,19 @@ class Team(models.Model):
     def __str__(self):
         return self.name
 
+
+class Player(models.Model):
+    name = models.CharField(max_length=30)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Match(models.Model):
+    teams = models.ManyToManyField(Team)
+    location = models.CharField(max_length=20)
+    score = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.location + ' ' + self.score
