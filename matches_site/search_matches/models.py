b437da5cc55093d8ed06aa45dacc3c34ce92cccc
Viacheslav <vkraskovsky@gmail.com>
2019-03-16 10:36:14 +0300

    somecommit

--- matches_site/search_matches/models.py
+++ matches_site/search_matches/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from datetime import date
+from datetime import datetime
 
 
 # Create your models here.
@@ -20,8 +22,9 @@ class Player(models.Model):
 
 class Match(models.Model):
     teams = models.ManyToManyField(Team)
+    match_date = models.DateField(default=date.today)
     location = models.CharField(max_length=20)
     score = models.CharField(max_length=5)
 
     def __str__(self):
-        return self.location + ' ' + self.score
+        return self.match_date.strftime('%m/%d/%Y') + '-' + self.location + ' ' + self.score
